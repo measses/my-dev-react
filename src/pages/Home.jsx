391f1d35eaf9d3d8e850eb66a00ce893c64805c5
Mert Araz <mertoreyiz1907@gmail.com>
2023-11-12 02:11:34 +0300

    home.jsx position fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Home = () => {
   return (
+    <div>
     <div className="flex justify-center items-start min-h-screen bg-gray-100 pt-20">
-      <div className="flex flex-wrap gap-4">
+      <div className="flex flex-wrap gap-5 ml-56">
         <div className="w-1/5 bg-white p-4 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300">
           <h2 className="text-xl font-semibold mb-4 text-center">
             Temel Bilgiler
@@ -33,7 +34,7 @@ const Home = () => {
         </div>
 
         <div className="w-1/5 bg-white p-4 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300">
-          <h2 className="text-xl font-semibold mb-4">Programlama Dilleri</h2>
+          <h2 className="text-xl font-semibold mb-4 text-center">Programlama Dilleri</h2>
           <div className="flex justify-center mb-4">
             <img
               src="../src/assets/img/programming.png"
@@ -60,7 +61,7 @@ const Home = () => {
         </div>
 
         <div className="w-1/5 bg-white p-4 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300">
-          <h2 className="text-xl font-semibold mb-4">
+          <h2 className="text-xl font-semibold mb-4 text-center">
             Çerçeveler ve Kütüphaneler
           </h2>
           <div className="flex justify-center mb-4">
@@ -71,7 +72,7 @@ const Home = () => {
             />
           </div>
           <h3 className="text-lg font-medium mb-4">
-            React, Angular, Vue gibi popüler bir frontend çerçevesini öğrenme.
+            React, Laravel, Node.js, Spring gibi Çerçeveler ve Kütüphaneler öğren.
           </h3>
           <a
             href="#_"
@@ -89,7 +90,7 @@ const Home = () => {
         </div>
 
         <div className="w-1/5 bg-white p-4 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300">
-          <h2 className="text-xl font-semibold mb-4">Veritabanları</h2>
+          <h2 className="text-xl font-semibold mb-4 text-center">Veritabanları</h2>
           <div className="flex justify-center mb-4">
             <img
               src="../src/assets/img/database.png"
@@ -115,6 +116,7 @@ const Home = () => {
           </a>
         </div>
       </div>
+    </div>
     </div>
   );
 };
